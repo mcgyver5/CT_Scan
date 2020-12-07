35ac2711845ddea73bff4e2c32c8cdb3c290374a
Tim <tim.e.mcguire@gmail.com>
2020-12-07 11:36:06 -0600

    Create TargetDomain.java

---
+++ model/TargetDomain.java
@@ -0,0 +1,14 @@
+public class TargetDomain{
+  private String IPAddress;
+  private String domain;
+  private boolean resolves;
+  private int httpStatus;
+  private int id;
+  private boolean selected;
+  private boolean inScope;
+  
+  public TargetDomain(){
+  
+  }
+
+}
